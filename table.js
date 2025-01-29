eeb1945f409faae3a64f4d5bc49001b6492072f5
scottorr-4 <orrs@oregonstate.edu>
2025-01-28 21:01:50 -0800

    Update table.js

--- table.js
+++ table.js
@@ -1,51 +1,45 @@
 document.addEventListener("DOMContentLoaded", function() {
-function make_table (a, b) { 
-    var table = document.createElement('table');
+    function make_table(a, b) { 
+        var table = document.createElement('table');
 
+        // Create a header row
+        var headerRow = document.createElement("tr");
 
-    //create a header row
-    var headerRow = document.createElement("tr");
+        // Add the "City" column
+        var cityHeader = document.createElement("th");
+        cityHeader.innerHTML = "City";
+        headerRow.appendChild(cityHeader);
 
-    //add the "City" column
-    var cityHeader = document.createElement("th");
-    cityHeader.innerHTML = "City";
-    headerRow.appendChild(cityHeader);
+        // Add the "Population" column
+        var popHeader = document.createElement("th");
+        popHeader.innerHTML = "Population";
+        headerRow.appendChild(popHeader);
 
-    //add the "Population" column
-    var popHeader = document.createElement("th");
-    popHeader.innerHTML = "Population";
-    headerRow.appendChild(popHeader);
+        // Add the row to the table
+        table.appendChild(headerRow);
 
-    //add the row to the table
-    table.appendChild(headerRow);
+        // Loop to add a new row for each city
+        for (var i = 0; i < a.length; i++) {
+            var tr = document.createElement("tr");
 
-    //loop to add a new row for each city
-    for (var i = 0; i < cities.length; i++){
-        var tr = document.createElement("tr");
+            var cityCell = document.createElement("td");
+            cityCell.innerHTML = a[i];
+            tr.appendChild(cityCell);
 
-        var city = document.createElement("td");
-        city.innerHTML = cities[i];
-        tr.appendChild(city);
+            var popCell = document.createElement("td");
+            popCell.innerHTML = b[i];
+            tr.appendChild(popCell);
 
-        var pop = document.createElement("td");
-        pop.innerHTML = population[i];
-        tr.appendChild(pop);
+            table.appendChild(tr);
+        }
 
-        table.appendChild(tr);
-    };
+        return table;
+    }
 
-    return table;
-}
+    var city = ['Corvallis', 'Portland', 'Eugene', 'Albany'];
+    var pop = ['59920', '652500', '176650', '56470'];
 
-var cities = ['Corvallis', 'Portland', 'Eugene', 'Albany']
-var pops = ['59920', '652500', '176650', '56470']
-
-
-make_table(cities, pops)
-
-
-    //add the table to the div in index.html
-    var myDiv =  document.getElementById("mydiv");
-    myDiv.appendChild(make_table(cities, pops));
+    // Add the table to the div in index.html
+    var myDiv = document.getElementById("myDiv");
+    myDiv.appendChild(make_table(city, pop));
 });
-
