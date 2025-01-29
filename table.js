c8222cd5b8cb9328fa71465ebc752ba719e75668
scottorr-4 <orrs@oregonstate.edu>
2025-01-28 20:55:50 -0800

    Update table.js

--- table.js
+++ table.js
@@ -1,46 +1,51 @@
-function make_table(a, b) {
+document.addEventListener("DOMContentLoaded", function() {
+function make_table (a, b) { 
     var table = document.createElement('table');
 
-    // Create a header row
+
+    //create a header row
     var headerRow = document.createElement("tr");
 
-    // Add the "City" column
+    //add the "City" column
     var cityHeader = document.createElement("th");
     cityHeader.innerHTML = "City";
     headerRow.appendChild(cityHeader);
 
-    // Add the "Population" column
+    //add the "Population" column
     var popHeader = document.createElement("th");
     popHeader.innerHTML = "Population";
     headerRow.appendChild(popHeader);
 
-    // Add the row to the table
+    //add the row to the table
     table.appendChild(headerRow);
 
-    // Loop to add a new row for each city
-    for (var i = 0; i < a.length; i++) {
+    //loop to add a new row for each city
+    for (var i = 0; i < cities.length; i++){
         var tr = document.createElement("tr");
 
-        var cityCell = document.createElement("td");
-        cityCell.innerHTML = a[i];
-        tr.appendChild(cityCell);
+        var city = document.createElement("td");
+        city.innerHTML = cities[i];
+        tr.appendChild(city);
 
-        var popCell = document.createElement("td");
-        popCell.innerHTML = b[i];
-        tr.appendChild(popCell);
+        var pop = document.createElement("td");
+        pop.innerHTML = population[i];
+        tr.appendChild(pop);
 
         table.appendChild(tr);
-    }
+    };
 
     return table;
 }
 
-var cities = ['Corvallis', 'Portland', 'Eugene', 'Albany'];
-var populations = ['59920', '652500', '176650', '56470'];
+var cities = ['Corvallis', 'Portland', 'Eugene', 'Albany']
+var pops = ['59920', '652500', '176650', '56470']
 
-var myTable = make_table(cities, populations);
 
-// Add the table to the div in index.html
-var myDiv = document.getElementById("mydiv");
-myDiv.appendChild(myTable);
+make_table(cities, pops)
+
+
+    //add the table to the div in index.html
+    var myDiv =  document.getElementById("mydiv");
+    myDiv.appendChild(make_table(cities, pops));
+});
 
